eadae44db5ee61ba864fa8349a9e2af8084b4129
roufrubel <rubel.ma2012@gmail.com>
2024-08-27 02:25:05 +0600

    vercel requrements added

--- index.js
+++ index.js
@@ -309,9 +309,9 @@ app.get('/order-stats', verifyToken, verifyAdmin, async (req, res) => {
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
